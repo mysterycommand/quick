ae79101b86022c622300c40418ea1cb153047302
Diogo Schneider <diogo.schneider@me.com>
2018-03-01 15:39:09 -0300

    Add font properties to TextSprite

--- src/quick.js
+++ src/quick.js
@@ -39,6 +39,157 @@
     RIGHT: 15
   };
 
+  const Color = makeSet([
+    "AliceBlue",
+    "AntiqueWhite",
+    "Aqua",
+    "Aquamarine",
+    "Azure",
+    "Beige",
+    "Bisque",
+    "Black",
+    "BlanchedAlmond",
+    "Blue",
+    "BlueViolet",
+    "Brown",
+    "BurlyWood",
+    "CadetBlue",
+    "Chartreuse",
+    "Chocolate",
+    "Coral",
+    "CornflowerBlue",
+    "Cornsilk",
+    "Crimson",
+    "Cyan",
+    "DarkBlue",
+    "DarkCyan",
+    "DarkGoldenRod",
+    "DarkGray",
+    "DarkGreen",
+    "DarkGrey",
+    "DarkKhaki",
+    "DarkMagenta",
+    "DarkOliveGreen",
+    "Darkorange",
+    "DarkOrchid",
+    "DarkRed",
+    "DarkSalmon",
+    "DarkSeaGreen",
+    "DarkSlateBlue",
+    "DarkSlateGray",
+    "DarkSlateGrey",
+    "DarkTurquoise",
+    "DarkViolet",
+    "DeepPink",
+    "DeepSkyBlue",
+    "DimGray",
+    "DimGrey",
+    "DodgerBlue",
+    "FireBrick",
+    "FloralWhite",
+    "ForestGreen",
+    "Fuchsia",
+    "Gainsboro",
+    "GhostWhite",
+    "Gold",
+    "GoldenRod",
+    "Gray",
+    "Green",
+    "GreenYellow",
+    "Grey",
+    "HoneyDew",
+    "HotPink",
+    "IndianRed",
+    "Indigo",
+    "Ivory",
+    "Khaki",
+    "Lavender",
+    "LavenderBlush",
+    "LawnGreen",
+    "LemonChiffon",
+    "LightBlue",
+    "LightCoral",
+    "LightCyan",
+    "LightGoldenRodYellow",
+    "LightGray",
+    "LightGreen",
+    "LightGrey",
+    "LightPink",
+    "LightSalmon",
+    "LightSeaGreen",
+    "LightSkyBlue",
+    "LightSlateGray",
+    "LightSlateGrey",
+    "LightSteelBlue",
+    "LightYellow",
+    "Lime",
+    "LimeGreen",
+    "Linen",
+    "Magenta",
+    "Maroon",
+    "MediumAquaMarine",
+    "MediumBlue",
+    "MediumOrchid",
+    "MediumPurple",
+    "MediumSeaGreen",
+    "MediumSlateBlue",
+    "MediumSpringGreen",
+    "MediumTurquoise",
+    "MediumVioletRed",
+    "MidnightBlue",
+    "MintCream",
+    "MistyRose",
+    "Moccasin",
+    "NavajoWhite",
+    "Navy",
+    "OldLace",
+    "Olive",
+    "OliveDrab",
+    "Orange",
+    "OrangeRed",
+    "Orchid",
+    "PaleGoldenRod",
+    "PaleGreen",
+    "PaleTurquoise",
+    "PaleVioletRed",
+    "PapayaWhip",
+    "PeachPuff",
+    "Peru",
+    "Pink",
+    "Plum",
+    "PowderBlue",
+    "Purple",
+    "RebeccaPurple",
+    "Red",
+    "RosyBrown",
+    "RoyalBlue",
+    "SaddleBrown",
+    "Salmon",
+    "SandyBrown",
+    "SeaGreen",
+    "SeaShell",
+    "Sienna",
+    "Silver",
+    "SkyBlue",
+    "SlateBlue",
+    "SlateGray",
+    "SlateGrey",
+    "Snow",
+    "SpringGreen",
+    "SteelBlue",
+    "Tan",
+    "Teal",
+    "Thistle",
+    "Tomato",
+    "Turquoise",
+    "Violet",
+    "Wheat",
+    "White",
+    "WhiteSmoke",
+    "Yellow",
+    "YellowGreen",
+  ]);
+
   const CommandEnum = {
     UP: 0,
     DOWN: 1,
@@ -51,6 +202,14 @@
     SELECT: 8,
     START: 9
   };
+
+  const FontFamily = makeSet([
+    "cursive",
+    "fantsy",
+    "monospace",
+    "sans-serif",
+    "serif",
+  ]);
 
   const KeyEnum = {
     ENTER: 13,
@@ -1706,9 +1865,8 @@
   class BaseTransition extends Sprite {
     constructor() {
       super();
-      const COLOR = 'Black';
       const FRAMES = 32;
-      this.setColor(COLOR);
+      this.setColor(Color.Black);
       this.setHeight(Quick.getHeight());
       this._increase = Quick.getWidth() / FRAMES;
     }
@@ -1801,16 +1959,56 @@
   class TextSprite extends Sprite {
     constructor(x, y, width, height, text) {
       super(x, y, width, height);
+      this.fontColor = Color.White;
       this.text = text;
+      this._fontFamily = FontFamily.monospace;
+      this._fontSize = 8;
+      this._updateFont();
     }
 
     render(context) {
-      Sprite.prototype.render.call(this, context) && context.fillText(this.text, this.x, this.y, this.width);
+      if (Sprite.prototype.render.call(this, context)) {
+        context.fillStyle = this.fontColor;
+        context.font = this._font;
+        context.fillText(this.text, this.x, this.y, this.width);
+      }
+    }
+
+    setFontFamily(fontFamily) {
+      this._fontFamily = fontFamily;
+      this._updateFont();
+      return this;
+    }
+
+    setFontSize(fontSize) {
+      this._fontSize = fontSize;
+      this._updateFont();
+      return this;
     }
 
     setText(text) {
       this.text = text;
       return this;
+    }
+
+    get fontFamily() {
+      return this._fontFamily;
+    }
+
+    get fontSize() {
+      return this._fontSize;
+    }
+
+    set fontFamily(fontFamily) {
+      this.setFontFamily(fontFamily);
+    }
+
+    set fontSize(fontSize) {
+      this.setFontSize(fontSize);
+    }
+
+    _updateFont() {
+      this._font = this._fontSize + 'px ' + this._fontFamily;
     }
   }
 
@@ -1928,6 +2126,17 @@
     canvas.style.height = height + 'px';
   }
 
+  function makeSet(array) {
+    const RESULT = {};
+
+    for (let i = 0; i < array.length; ++i) {
+      const VALUE = array[i];
+      RESULT[VALUE] = VALUE;
+    }
+
+    return RESULT;
+  }
+
   function toDegrees(radians) {
     return radians * 180 / Math.PI;
   }
@@ -1940,8 +2149,10 @@
     Animation,
     BaseTile,
     BaseTransition,
+    Color,
     CommandEnum,
     Controller,
+    FontFamily,
     FontSprite,
     Frame,
     ImageFactory,
